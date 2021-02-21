04566fdc45cbddcd3a60de4ef219055450d42a2c
Ramya Iyer <ramyaiyer1000@gmail.com>
2021-02-21 13:06:24 -0600

    uploading the most recent code

--- to-do-animate.js
+++ to-do-animate.js
@@ -1,8 +1,7 @@
 
   // Click on a close button to hide the current list item
   var close = document.getElementsByClassName("close");
-  var i;
-  for (i = 0; i < close.length; i++) {
+  for (var i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
@@ -30,7 +29,7 @@
     }
     document.getElementById("myInput").value = "";
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    var txt = document.createTextNode("Delete");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
